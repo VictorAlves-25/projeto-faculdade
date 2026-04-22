4f1394948d6ebd386877f3a26f841c852a09e4ee
VictorAlves-25 <joaovictras2502@gmail.com>
2026-04-22 20:23:02 -0300

    app.py

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    return a / b
+
+def saudacao(nome):
+    return f"Olá, {nome}!"
+
 @app.route("/")
 def home():
-    return "Aplicacao rodando com sucesso no Docker!"
+    return "Aplicacao rodando com sucesso"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
